1a94d0c3f621c9f927da7035991a01d992530376
Anji <anzhi0708@gmail.com>
2025-04-19 20:46:35 +0900

    fix: LD_LIBRARY_PATH

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,9 +1,7 @@
 # This bash script is written to show the commands used
 
-cd ./src/c_source
-
 # 1. Builds dylib
-clang -fPIC -shared hello.c -o libhello.so
+cc -fPIC -shared ./src/c_source/hello.c -o ./libhello.so
 
 # 2. Cargo
 cargo build
@@ -12,10 +10,12 @@ cargo build
 # Adds `c_source/` to dylib search path (-L)
 #
 # Linux
-export LD_LIBRARY_PATH=./src/c_source:$(LD_LIBRARY_PATH)
+# export LD_LIBRARY_PATH=$(pwd)/src/c_source:$(LD_LIBRARY_PATH)
+export LD_LIBRARY_PATH=$(pwd):$(LD_LIBRARY_PATH)
 #
 # MacOS
-export DYLD_LIBRARY_PATH=./src/c_source:$(LD_LIBRARY_PATH)
+# export DYLD_LIBRARY_PATH=$(pwd)/src/c_source:$(DYLD_LIBRARY_PATH)
+export DYLD_LIBRARY_PATH=$(pwd):$(DYLD_LIBRARY_PATH)
 
 
 cargo run
